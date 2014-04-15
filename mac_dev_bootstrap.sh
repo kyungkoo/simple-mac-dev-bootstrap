a497677e8d01e129f086996a347cd19fb7e9da31
Kyungkoo Kang <kyungkoo.kang@yahoo.com>
2014-04-15 11:04:54 +0900

    add vimrc download code

--- mac_dev_bootstrap.sh
+++ mac_dev_bootstrap.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 
 # install homebrew
+echo "Install Homebrew"
 ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
 
-# install homebrew fomula
+# install homebrew formula
 # python, nodejs, vim, tree, git, gradle, tmux
+echo "Install formula"
 brew install python node vim tree git gradle tmux
+
+vimfile="$HOME/.vimrc"
+
+if [ -f $vimfile ]; then
+    echo "vimrc already exist!"
+else
+    echo "start download vimrc using curl"
+    curl -o $HOME/.vimrc https://raw.githubusercontent.com/kyungkoo/simple-mac-dev-bootstrap/master/vimrc
+fi
