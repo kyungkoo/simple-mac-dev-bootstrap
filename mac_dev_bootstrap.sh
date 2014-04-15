ab7de5b5e897f201c510179169605bb2a318d9ae
Kyungkoo Kang <kyungkoo.kang@yahoo.com>
2014-04-15 10:14:17 +0900

    add script

---
+++ mac_dev_bootstrap.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# install homebrew
+ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
+
+# install homebrew fomula
+# python, nodejs, vim, tree, git, gradle, tmux
+brew install python node vim tree git gradle tmux
