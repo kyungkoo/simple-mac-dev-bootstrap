1286e3c5fcb9d0028a6d1c9a594f078ef0098142
Kyungkoo Kang <kyungkoo.kang@yahoo.com>
2014-04-15 12:58:36 +0900

    add command line tools install script

--- mac_dev_bootstrap.sh
+++ mac_dev_bootstrap.sh
@@ -1,19 +1,33 @@
 #!/bin/bash
 
-# install homebrew
-echo "Install Homebrew"
-ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
+# command line tool directory
+cmd_line_tools_dir="/Library/Developer/CommandLineTools"
 
-# install homebrew formula
-# python, nodejs, vim, tree, git, gradle, tmux
-echo "Install formula"
-brew install python node vim tree git gradle tmux
+# check command line tools installed
+if [ -d $cmd_line_tools_dir ]; then
+    # install xcode command line tools
+    echo "Xcode CommandLineTools already installed!"
+    # install homebrew
+    echo "Install Homebrew"
+    ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
 
-vimfile="$HOME/.vimrc"
+    # install homebrew formula
+    # python, nodejs, vim, tree, git, gradle, tmux
+    echo "Install formula"
+    brew install python node vim tree git gradle tmux
 
-if [ -f $vimfile ]; then
-    echo "vimrc already exist!"
+    # vimrc file path
+    vimfile="$HOME/.vimrc"
+
+    # check vimrc file exist
+    if [ -f $vimfile ]; then
+        echo "vimrc already exist!"
+    else
+        echo "start download vimrc using curl"
+        curl -o $HOME/.vimrc https://raw.githubusercontent.com/kyungkoo/simple-mac-dev-bootstrap/master/vimrc
+    fi
 else
-    echo "start download vimrc using curl"
-    curl -o $HOME/.vimrc https://raw.githubusercontent.com/kyungkoo/simple-mac-dev-bootstrap/master/vimrc
+    echo "install command line tools after install it, please reexcute this script"
+    echo "Install xcode command line tools"
+    xcode-select --install
 fi
